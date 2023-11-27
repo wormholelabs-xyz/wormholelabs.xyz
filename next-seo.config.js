38b2d3f266d8e0f284a1e9e53afaa99c1fac10f2
Adrian Timpau <timpau@users.noreply.github.com>
2023-11-28 01:18:46 +0200

    add seo

---
+++ next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+  defaultTitle: "Wormhole Labs",
+  titleTemplate: "%s | Wormhole Labs",
+  description:
+    "At Wormhole Labs, we're committed to forging a future where interoperability and security lead the blockchain revolution. We envision a boundaryless blockchain world, and we’re working together to make it a reality.",
+  openGraph: {
+    type: "website",
+    images: [
+      {
+        url: "default-banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Wormhole Labs",
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@wormholecrypto",
+    cardType: "summary_large_image",
+  },
+};
